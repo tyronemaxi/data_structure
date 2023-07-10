a2c2a76eb990e52184900597e3e6be0cf6380f59
tyronemaxi <tyronemaxi@163.com>
2023-07-10 15:31:05 +0800

    [FEATURE] leetcode 215 add

---
+++ leetcode/排序类/215_findKthLarget.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: tyrone
+File: 215_findKthLarget.py
+Time: 2023/7/10
+"""
+import random
+# https://leetcode.cn/problems/kth-largest-element-in-an-array/
+# 解题参考：https://www.bilibili.com/video/BV1A5411H7zr/?spm_id_from=333.337.search-card.all.click&vd_source=67c3c7f430d599a6b5dacd9127487fc7
+from typing import List
+
+# 快排 partition
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int):
+        return self.quick_sort(nums, 0, len(nums) - 1, k)
+
+    def quick_sort(self, nums, l, r, k):
+        index = self.random_pivot(nums, l, r)
+        if index == k - 1:
+            return nums[index]
+
+        elif index > k - 1:
+            return self.quick_sort(nums, l, index - 1, k)
+
+        else:
+            return self.quick_sort(nums, index + 1, r, k)
+
+    def random_pivot(self, nums, l, r):
+        i = random.randint(l, r)
+        nums[i], nums[r] = nums[r], nums[i]
+        return self.partition(nums, l, r)
+
+    def partition(self, nums, l, r):
+        pivot = nums[r]
+        right = r
+        while l <= r:
+            while l <= r and nums[l] > pivot:
+                l += 1
+
+            while l <= r and nums[r] <= pivot:
+                r -= 1
+
+            if l <= r:
+                nums[l], nums[r] = nums[r], nums[l]
+
+        nums[l], nums[right] = nums[right], nums[l]
+
+        return l
+
+# 堆
+from heapq import heapify, heappush, heappop
+
+class Solution2:
+    def findKthLargest(self, nums: List[int], k: int):
+        min_heap = []
+        heapify(min_heap)
+        for num in nums:
+            heappush(min_heap, num)
+            if len(min_heap) > k:
+                heappop(min_heap)
+        return min_heap[0]
---
+++ leetcode/排序类/sort.md
@@ -0,0 +1,17 @@
+# leet code 215
+> 数组中第 K 个最大元素
+## 题目描述
+https://leetcode.cn/problems/kth-largest-element-in-an-array/
+
+## 解题思路
+### 暴力法
+- 使用内置排序函数 `sort()` 从小到大
+- 返回倒数第 k 个元素：returns nums[len(nums) - k]
+### 快速排序的 partition
+- 取随机 pivot 基准值
+- 分区 partition, 大｜pivot|小
+- 返回第 k 大的元素
+### 堆
+- 堆是一个完全二叉树
+- 堆中的每一个节点的值都必须大于等于（或小于等于）其子树中每个节点的值
+
